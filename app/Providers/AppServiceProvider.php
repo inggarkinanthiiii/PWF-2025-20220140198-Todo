9b5f0a428f5e716b3e5aa338734e4bb9704dcf30
InggarKinanthi <cherlita0505@gmail.com>
2025-03-18 17:30:08 +0700

    env, mvc, route, relationship

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View; // Tambahkan ini
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Tambahkan lokasi views secara eksplisit (Opsional)
+        View::addLocation(resource_path('views'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TodoController; 
+use App\Http\Controllers\UserController; 
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +17,17 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // ✅ Rute Todo
+    Route::get('/todo', [TodoController::class, 'index'])->name('todo.index');
+    Route::get('/todo/create', [TodoController::class, 'create'])->name('todo.create');
+    Route::post('/todo', [TodoController::class, 'store'])->name('todo.store'); // Simpan
+    Route::get('/todo/edit/{id}', [TodoController::class, 'edit'])->name('todo.edit'); // Edit
+    Route::put('/todo/{id}', [TodoController::class, 'update'])->name('todo.update'); // Update
+    Route::delete('/todo/{id}', [TodoController::class, 'destroy'])->name('todo.destroy'); // Hapus
+
+    // ✅ Rute User
+    Route::get('/user', [UserController::class, 'index'])->name('user.index');
 });
 
 require __DIR__.'/auth.php';
